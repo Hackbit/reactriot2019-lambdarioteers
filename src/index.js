257e552dc92742f6d21974df5436904177a98b9b
cecil john tantay <cjbtantay@gmail.com>
2019-09-27 20:25:39 -0700

    add localstorage persistence

--- src/index.js
+++ src/index.js
@@ -10,8 +10,14 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
 
+
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
+
+store.subscribe(()=>{
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
