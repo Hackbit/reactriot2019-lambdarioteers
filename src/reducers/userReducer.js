3be6a36aadd6053bd6c01b87f6c4d44bf1fa2f2b
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-28 05:48:51 +0000

    remove redundant `break`

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,28 +1,26 @@
-import { DELETING_USER, ADDING_USER } from '../actions';
+import { DELETING_USER, ADDING_USER } from "../actions";
 
 const initial_state = {
-	users: [],
-	addingUser: false,
-	deletingUser: false,
-	addingUserError: false
+  users: [],
+  addingUser: false,
+  deletingUser: false,
+  addingUserError: false
 };
 
 const userReducer = (state = initial_state, action) => {
-	let newUsers;
+  let newUsers;
 
-	console.log("adding user")
+  console.log("adding user");
 
-	switch(action.type) {
-		case ADDING_USER:
-			return { ...state, users: [...state.users, action.payload ]};
-			break;
-		case DELETING_USER:
-			newUsers = state.users.filter(user => (user.id != action.payload));
-			return { ...state, users: [...newUsers]}
-			break;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case ADDING_USER:
+      return { ...state, users: [...state.users, action.payload] };
+    case DELETING_USER:
+      newUsers = state.users.filter(user => user.id !== action.payload);
+      return { ...state, users: [...newUsers] };
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
