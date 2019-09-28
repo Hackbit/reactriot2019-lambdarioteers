353f36a374492347eb5b330a6d0b48233974e06d
cecil john tantay <cjbtantay@gmail.com>
2019-09-27 21:26:08 -0700

    add crud actions and reducer

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -1,4 +1,8 @@
 export const ADD_TASK = "ADD_TASK"
+export const DELETE_TASK = "DELETE_TASK"
+export const POPULATE_FORM = "POPULATE_FORM"
+export const UPDATE_TASK = "UPDATE_TASK"
+export const CANCEL = "CANCEL"
 
 export const addTask = (task) => {
   return {
@@ -6,3 +10,26 @@ export const addTask = (task) => {
     payload: task
   }
 }
+export const deleteTask = id => {
+  return {
+    type: DELETE_TASK,
+    payload: id
+  }
+};
+export const populateForm = (task) => {
+  return {
+    type: POPULATE_FORM,
+    payload: task
+  }
+};
+export const updateTask = (task) => {
+  return {
+    type: UPDATE_TASK,
+    payload: task
+  }
+};
+export const cancel = () => {
+  return {
+    type: CANCEL
+  }
+};
--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TASK } from '../actions/taskActions'
+import { ADD_TASK, DELETE_TASK, POPULATE_FORM, UPDATE_TASK, CANCEL } from '../actions/taskActions'
 
 const initialState = {
   tasks: [
@@ -10,7 +10,13 @@ const initialState = {
       pointsToEarn: 25,
       img: ""
     }
-  ]
+  ],
+  isUpating: false,
+  name: "",
+  locationInput: "",
+  time: "",
+  pointsToEarn: null,
+  img: ""
 }
 
 const taskReducer = (state = initialState, action) => {
@@ -19,6 +25,44 @@ const taskReducer = (state = initialState, action) => {
       return {
         ...state,
         tasks: [...state.tasks, action.payload]
+      }
+    case DELETE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => task.id !== action.payload)
+      }
+    case POPULATE_FORM:
+      return {
+        ...state,
+        isUpdating: true,
+        name: action.payload.name,
+        locationInput: action.payload.locationInput,
+        time: action.payload.time,
+        pointsToEarn: action.payload.pointsToEarn,
+        img: action.payload.img
+      }
+    case UPDATE_TASK:
+      return {
+        ...state,
+        isUpdating: false,
+        tasks: state.tasks.map(task => {
+          if (task.id === action.payload.id) {
+            return {
+              ...task,
+              name: action.payload.name,
+              locationInput: action.payload.locationInput,
+              time: action.payload.time,
+              pointsToEarn: action.payload.pointsToEarn,
+              img: action.payload.img
+            }
+          }
+          return task
+        })
+      }
+    case CANCEL:
+      return {
+        ...state,
+        isUpdating: false
       }
     default:
       return state
