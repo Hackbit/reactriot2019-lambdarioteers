680ca9d20fe906ad4994e82e9800ad766fe3b295
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-27 20:33:18 -0500

    General layout of register form with Yup form validation, and React Router implemeneted.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 
 import Register from "./components/Register";
@@ -6,7 +7,7 @@ import Register from "./components/Register";
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Route path="/register" component={Register} />
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 import styled from "styled-components";
 
 const FormContainer = styled.div`
@@ -9,6 +10,11 @@ const FormContainer = styled.div`
     flex-direction: column;
     width: 100%;
     height: 100vh;
+    background: #50a2a7;
+
+    h1 {
+        font-size: 2.2rem;
+    }
 
     form {
         display: flex;
@@ -16,27 +22,47 @@ const FormContainer = styled.div`
         justify-content: space-evenly;
         align-items: center;
         width: 80%;
-        height: 400px;
 
         input {
             width: 70%;
             padding: 8px;
-            border: 2px solid lightgrey;
+            margin: 12px 0;
+            border: 2px solid transparent;
             border-radius: 3px;
-        }
-
-        button {
-            border: none;
-            border-radius: 3px;
-            width: 74%;
-            padding: 10px;
-            background: pink;
-        }
+            font-size: 1rem;
+            background: #e4d6a7;
+        }   
     }
-
 `;
 
-const Register = () => {
+const SubmitButton = styled.button`
+    border: none;
+    border-radius: 3px;
+    width: 76%;
+    padding: 14px;
+    background: pink;
+    font-size: 1rem;
+    background: #e9b44c;
+    transition: all .3s;
+    margin: 12px 0;
+
+    &:hover {
+        background: #9b2915;
+        color: white;
+    }
+`;
+
+const InputError = styled.p`
+    font-size: .8rem;
+    background: #9b2915;
+    color: white;
+    width: 73%;
+    border-radius: 3px;
+    padding: 3px 0 3px 8px;
+    margin-top: -3px;
+`;
+
+const Register = ({ errors, touched, isSubmitting }) => {
     return (
         <FormContainer>
             <h1>Register</h1>
@@ -46,37 +72,68 @@ const Register = () => {
                     type="text" 
                     placeholder="Organization"
                 />
+                {errors.organization && <InputError>{errors.organization}</InputError>}
                 <Field 
                     name="name" 
                     type="text" 
                     placeholder="Contact Name"
                 />
+                {errors.name && <InputError>{errors.name}</InputError>}
                 <Field 
                     name="phone" 
                     type="phone" 
                     placeholder="Phone"
                 />
+                {errors.phone && <InputError>{errors.phone}</InputError>}
+                {touched.phone && <InputError>{touched.phone}</InputError>}
                 <Field 
                     name="email" 
                     type="email" 
                     placeholder="E-mail"
                 />
+                {errors.email && <InputError>{errors.email}</InputError>}
                 <Field 
                     name="address" 
                     type="address" 
                     placeholder="Address"
                 />
-                <button>Submit</button>
+                {errors.address && <InputError>{errors.address}</InputError>}
+                <SubmitButton disabled={isSubmitting} type="submit">Register</SubmitButton>
             </Form>
         </FormContainer>
     );
 };
 
 const FormikRegisterForm = withFormik({
-    mapPropsToValues({ organization }){
+    mapPropsToValues({ organization, name, phone, email, address }){
         return {
-            organization: organization || ""
+            organization: organization || "",
+            name: name || "",
+            phone: phone || "",
+            email: email || "",
+            address: address || ""
         }
+    }, 
+
+    validationSchema: Yup.object().shape({
+        organization: Yup.string()
+            .required("Please enter an organization."),
+        name: Yup.string()
+            .required("Please enter a contact name."),
+        phone: Yup.number()
+            .typeError("Please enter a valid phone number.")
+            .required("Please enter a phone number."),
+        email: Yup.string()
+            .required("Please enter an e-mail."),
+        address: Yup.string()
+            .required("Please enter an address.")
+    }),
+
+    handleSubmit(values, { resetForm }){
+        console.log(values)
+        values.name = "";
+        values.phone = "";
+        resetForm();
     }
 })(Register);
 
