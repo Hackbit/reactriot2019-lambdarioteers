6495effff63f3d0a781ace52b6022425e4632b7a
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-28 05:58:37 +0000

    render `img` when image url is provided

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Task = ({ task, history }) => {
-  const { id, name, locationInput, time, pointsToEarn, img } = task
+  const { id, name, locationInput, time, pointsToEarn, img } = task;
   return (
-    <TaskContainer onClick={() => {
-      history.push(`/task/${id}`)
-    }}>
+    <TaskContainer
+      onClick={() => {
+        history.push(`/task/${id}`);
+      }}
+    >
       <h1>{name}</h1>
       <p>{locationInput}</p>
       <p>{time}</p>
       <p>{pointsToEarn}</p>
-      <img src={img} alt="img url"/>
+      {img !== "" && <img src={img} alt="img url" />}
     </TaskContainer>
-  )
-}
+  );
+};
 
 const TaskContainer = styled.div`
   border: 1px solid black;
   padding: 10px;
   cursor: pointer;
   margin-bottom: 30px;
-`
+`;
 
-export default Task
-
+export default Task;
